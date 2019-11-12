c26b1a918a93ab6a2b3db34a58184475918f75e8
Hanani Rafei <hanani@zen.com.my>
2019-11-12 14:27:31 +0800

    Intergrate user quota popup page with API

--- src/app/pages/subscriptions/subscriber-details/update-user-numbers/update-user-numbers.component.ts
+++ src/app/pages/subscriptions/subscriber-details/update-user-numbers/update-user-numbers.component.ts
@@ -1,4 +1,9 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+
+import { APIService } from '../../../../services/shared-service/api.service';
+import { InfoPopupService } from '../../../../layout/notificationPopup/info-popup.service';
+
 import { subscriberUpdateInfo, subsDtlPopoverCtrlr} from '../subscriber-details.page';
 
 /**
@@ -16,9 +21,14 @@ export class UpdateUserNumbersComponent implements OnInit {
 
   /**
    * Creates an instance of UpdateUserNumbersComponent.
+   * @param {APIService} updQuotaAPISvs This property is to get methods from APIService
+   * @param {InfoPopupService} updQuotaInfoPopup This property is to get methods from InfoPopupService
    * @memberof UpdateUserNumbersComponent
    */
-  constructor() { }
+  constructor(
+    private updQuotaAPISvs: APIService,
+    private updQuotaInfoPopup: InfoPopupService
+  ) { }
 
   /**
    * This property is to get current subscriber's data
@@ -46,8 +56,8 @@ export class UpdateUserNumbersComponent implements OnInit {
   ngOnInit() {
     this.selectedSubscriberInfo = subscriberUpdateInfo;
     this.updateClientData = {...this.selectedSubscriberInfo, progressBarValue:
-                      this.selectedSubscriberInfo.employeeNumber / this.selectedSubscriberInfo.employeeQuota};
-    this.updateEmployeeQuota = this.selectedSubscriberInfo.employeeQuota;
+      this.selectedSubscriberInfo.USED_QUOTA / this.selectedSubscriberInfo.USED_QUOTA};
+    this.updateEmployeeQuota = this.selectedSubscriberInfo.QUOTA;
   }
 
   /**
@@ -55,8 +65,24 @@ export class UpdateUserNumbersComponent implements OnInit {
    * @memberof UpdateUserNumbersComponent
    */
   saveChanges() {
-    this.updateClientData.progressBarValue = this.updateClientData.employeeNumber / this.updateClientData.employeeQuota;
+    this.updateClientData.progressBarValue = this.updateClientData.USED_QUOTA / this.updateClientData.QUOTA;
     Object.assign(this.selectedSubscriberInfo,  this.updateClientData);
+    const reqUpdate = {
+      'customerGuid': this.selectedSubscriberInfo['CUSTOMER_GUID'],
+      'subscriptionPlan': this.selectedSubscriberInfo['PLAN'],
+      'subscriptionStatus': this.selectedSubscriberInfo['STATUS'],
+      'subscriptionQuota': this.selectedSubscriberInfo['QUOTA'],
+      'usedQuota': this.selectedSubscriberInfo['USED_QUOTA'],
+      'activationDate': this.selectedSubscriberInfo['ACTIVATION_DATE'],
+      'lastBillingDate': this.selectedSubscriberInfo['LAST_BILLING_DATE'],
+      'nextBillingDate': this.selectedSubscriberInfo['NEXT_BILLING_DATE'],
+      'recurrInterval': this.selectedSubscriberInfo['RECURR_INTERVAL'],
+      'recurrIntervalVal': this.selectedSubscriberInfo['RECURR_INTERVAL_VAL'],
+      'billingCycle': this.selectedSubscriberInfo['BILLING_CYCLE'],
+      'subscriptionGuid': this.selectedSubscriberInfo['SUBSCRIPTION_GUID']
+    };
+
+    this.updateUserQuota(reqUpdate);
     this.dissmissPopup();
   }
 
@@ -67,6 +93,29 @@ export class UpdateUserNumbersComponent implements OnInit {
    */
   async dissmissPopup() {
     return await subsDtlPopoverCtrlr.dismiss();
+  }
+
+  /**
+   * Thie method is to set alert popup from returned value
+   * @param {*} obj This parameter is to pass object to patch data into API
+   * @memberof UpdateUserNumbersComponent
+   */
+  updateUserQuota(obj) {
+    this.patchUpdateUserQuota(obj).subscribe(
+      patchRespond => {
+        this.updQuotaInfoPopup.alertPopup('You have successfully update user quota!', 'alert-success');
+      }
+    );
+  }
+
+  /**
+   * This method is to send request to patch API to update susbcriptions
+   * @param {*} obj This parameter is to pass object to patch data into API
+   * @returns {Observable<any>}
+   * @memberof UpdateUserNumbersComponent
+   */
+  patchUpdateUserQuota(obj): Observable<any> {
+    return this.updQuotaAPISvs.patchApi(obj,'/api/admin/subscription');
   }
 
 }
