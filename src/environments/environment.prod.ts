a3068f08ceb0d6fa48a904990502763cc6608c55
Hanani Rafei <hanani@zen.com.my>
2020-08-12 15:40:23 +0800

    change root api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   URL_BASE: 'https://tenant.beesuite.app',
-  URL_API: 'https://tenantcore.beesuite.app',
-  // URL_API: 'https://tenantcore.beesuite.app/api/docs/',
+  // URL_API: 'https://tenantcore.beesuite.app',
+  URL_API: 'https://tenantcore.beesuite.app/api/docs/',
   URL_FORGOTPASSWORD: 'https://resetpassword.beesuite.app'
 };
